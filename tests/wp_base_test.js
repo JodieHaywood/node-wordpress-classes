c84ac35e7986edafad36bacb235faf942ad26e10
Lingliang Zhang <lingliangz@gmail.com>
2015-05-25 18:13:45 +1200

    some early tests

--- tests/wp_base_test.js
+++ tests/wp_base_test.js
@@ -1,1 +1,8 @@
-jest.dontMock("")
+jest.dontMock("../lib/WP_Base");
+
+describe("test", function() {
+  it("works", function() {
+    expect(true).toEqual(true);
+    expect(true).toEqual(false);
+  });
+});
